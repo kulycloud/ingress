78d1bccfef1ac82f072b6f1eadc9b9299f97510d
themmj <themmj@users.noreply.github.com>
2020-11-18 00:04:48 +0100

    KU-59 Add handler logic

--- servers/handler.go
+++ servers/handler.go
@@ -1,16 +1,40 @@
 package servers
 
 import (
+	commonHttp "github.com/kulycloud/common/http"
+	"github.com/kulycloud/ingress/communication"
 	"net/http"
 )
 
 func registerHandler() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// parse request to grpc
-		// get route step and send foreward
-		// somehow write back response
-
-		// for now:
-		w.Write([]byte("Ping"))
+		if communication.Storage.Ready() {
+			routeStart, err := communication.Storage.GetRouteStart(r.Context(), r.Host)
+			if err != nil {
+				logger.Errorw("storage error", "error", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			com := commonHttp.NewHttpCommunicator(routeStart.GetEndpoints())
+			stream, err := com.Stream(r.Context())
+			if err != nil {
+				logger.Errorw("did not get stream", "error", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			err = sendRequest(stream, r, routeStart)
+			if err != nil {
+				logger.Errorw("error while streaming request", "error", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			err = writeResponse(stream, w)
+			if err != nil {
+				logger.Errorw("error while streaming response", "error", err)
+			}
+		} else {
+			logger.Error("storage not ready")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	})
 }
